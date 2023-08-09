7e6b0895f3b9ed317c3c8ec5f57308f0a04dd72f
MMehrad <teach2049@gmail.com>
2023-08-09 14:25:16 +0330

    Separate html and css files were merged and the option to change the language was added as a style change

--- assets/jquery/customjq.js
+++ assets/jquery/customjq.js
@@ -2,12 +2,11 @@
 //open-search-window-and-clearbtn-txt
 $(".opn-search").on("click", function () {
     $(".search-box").addClass("open", 1000);
-    $(".clear").hide();
     $('input').keyup(function(){
         if($.trim(this.value).length > 0)
-            $(".clear").show()
+            $(".clear").show();
          else
-            $(".clear").hide()
+            $(".clear").hide();
      });
 
      $(".clear").on("click", function () {
@@ -20,3 +19,16 @@ $(".close").on("click", function () {
     $(".search-box").removeClass("open", 1000);
 });
 
+
+
+//change lang
+$(".change-lang").on("click", function () {
+    if($(".change-lang").text().trim() == "change(rtl)")
+    {
+        $("#lang-style").attr('href','assets/css/style-rtl.css');
+        $(".change-lang").text("change(ltr)");
+    }else{
+        $("#lang-style").attr('href','assets/css/style-ltr.css');
+        $(".change-lang").text("change(rtl)");
+    }
+});
