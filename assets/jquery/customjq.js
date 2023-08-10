8731287ded624e888756b0f8fc94a4d35c591082
MMehrad <teach2049@gmail.com>
2023-08-10 14:55:57 +0330

    A number of bugs were fixed

--- assets/jquery/customjq.js
+++ assets/jquery/customjq.js
@@ -2,17 +2,18 @@
 //open-search-window-and-clearbtn-txt
 $(".opn-search").on("click", function () {
     $(".search-box").addClass("open", 1000);
-    $('input').keyup(function(){
-        if($.trim(this.value).length > 0)
-            $(".clear").show();
-         else
-            $(".clear").hide();
-     });
+    $('input').focus();
+    if($('input').text().trim().length > 0)
+    {
+        $(".clear").show()
+    }else
+    {
+        $(".clear").hide()
+    }
 
      $(".clear").on("click", function () {
         $(".clear").hide();
      });
-
 });
 
 $(".close").on("click", function () {
@@ -27,8 +28,10 @@ $(".change-lang").on("click", function () {
     {
         $("#lang-style").attr('href','assets/css/style-rtl.css');
         $(".change-lang").text("change(ltr)");
+        $('input').attr("placeholder", "جستجو ...");
     }else{
         $("#lang-style").attr('href','assets/css/style-ltr.css');
         $(".change-lang").text("change(rtl)");
+        $('input').attr("placeholder", "Search ...");
     }
 });
